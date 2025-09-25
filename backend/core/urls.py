2292f55ec0bc19e3c561a3057eb0ed9a1a8da05d
theostamp <theostamp@example.com>
2025-09-26 00:37:33 +0300

    kiosk evolution 2

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
    
-from .views import get_csrf_token, api_root
+from .views import get_csrf_token, api_root, community_messages
 
 urlpatterns = [
     path("", api_root, name="api_root"),
     path("csrf/", get_csrf_token, name="get_csrf_token"),
+    path("community-messages/", community_messages, name="community_messages"),
 ]
